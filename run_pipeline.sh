6242af659ef8c35768b4fcc1ca42865671d59dfe
Jędrzej Kubica <jj.kubica@student.uw.edu.pl>
2022-03-10 20:31:14 +0100

    Add files via upload

---
+++ run_pipeline.sh
@@ -0,0 +1,43 @@
+#!bin/bash
+
+#downloading the SRA toolkit
+wget https://ftp-trace.ncbi.nlm.nih.gov/sra/sdk/2.9.1-1/sratoolkit.2.9.1-1-centos_linux64.tar.gz
+
+tar -xzvf sratoolkit.2.9.1-1-centos_linux64.tar.gz
+
+export PATH=$PWD/sratoolkit.2.9.1-1-centos_linux64/bin/:${PATH}
+
+mkdir sraDir
+
+vdb-config -i
+
+#downloading the SRA files
+prefetch SRR17233493
+prefetch SRR17233501
+prefetch SRR17233553
+prefetch SRR17233557
+prefetch SRR17233561
+prefetch SRR17233653
+prefetch SRR17233670
+prefetch SRR17233675
+prefetch SRR17233693
+prefetch SRR17233717
+prefetch SRR17233746
+prefetch SRR17233747
+prefetch SRR17233752
+prefetch SRR17233760
+prefetch SRR17233766
+prefetch SRR17233775
+prefetch SRR17233787
+
+#extracting fastq files
+mkdir fastq
+
+for sraFile in ncbi/public/sra/*.sra; do
+ echo "Extracting fastq from "${sraFile}
+ fastq-dump \
+    --origfmt \
+    --gzip \
+    --outdir fastq \
+    ${sraFile}
+done
